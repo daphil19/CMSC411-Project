954efa23f0bc97c080d2ab913a9890ac0b2c0566
katiebbuck <katieb5@umbc.edu>
2017-04-26 12:29:13 -0400

    Add files via upload

---
+++ cordic_alg/cordic.v
@@ -0,0 +1,33 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:33:54 04/26/2017 
+// Design Name: 
+// Module Name:    cordic 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module cordic(
+    input [32:0] x_in,
+    input [32:0] k_in,
+    input [32:0] y_in,
+    input [32:0] e_k_in,
+    input CE,
+    output [32:0] n_out,
+    output done,
+    output [32:0] y_out
+    );
+
+
+endmodule
---
+++ cordic_alg/cordic_LUT.v
@@ -0,0 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:36:05 04/26/2017 
+// Design Name: 
+// Module Name:    cordic_LUT 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module cordic_LUT(
+    input [32:0] x_in,
+	 input [4:0] addr,
+    output [32:0] reg k_out,
+    output [32:0] reg e_k_out,
+	 output [4:0] reg addr_out,
+    input CE
+    );
+	 
+	 reg [31:0] LUT_k [0:31];
+	 reg [31:0] LUT_e_k [0:31];
+	 
+	 parameter LAST = 32;
+	 
+	 initial begin
+		$readmemh("", LUT_k);
+		$readmemh("", LUT_e_k);
+	 end
+	 
+	 always @ (posedge CE) begin //on enable
+		if (addr == LAST) begin //hits last address..done
+				k_out <= LUT_k[addr];
+				e_k_out <= LUT_e_k[addr];
+				addr_out <= addr;
+		end else begin
+			if (LUT_k[addr] <= x_in) begin
+				k_out <= LUT_k[addr];
+				e_k_out <= LUT_e_k[addr];
+				addr_out <= addr;
+			end else
+			
+				addr <= addr + 1; //increase count
+			end
+		end
+	 
+	 end
+
+
+endmodule
